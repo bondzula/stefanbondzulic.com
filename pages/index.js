384e356a349747d8459693ec5ea024d21c4c4851
Stefan Bondzulic <stefanbondzulic@pm.com>
2021-04-19 01:18:31 +0200

    feat - Adds tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className="bg-gray-100">
       <Head>
         <title>Stefan Bondzulic</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
+      <main className="min-h-screen flex justify-center items-center flex-col">
+        <h1 className="text-4xl mb-8 font-semibold">
           Welcome to my personal site
         </h1>
 
-        <p className={styles.description}>My name is Stefan, and this is just a simple test of serverless framework</p>
+        <p>My name is Stefan, and this is just a simple test of serverless framework</p>
       </main>
     </div>
   )
