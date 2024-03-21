4b0f21571bb0844c8650577bbfe45d1e99b70ee3
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-03-21 18:51:35 +0100

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys =
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... }@inputs:
+    let forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+      });
+
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = devenv.lib.mkShell {
+            inherit inputs pkgs;
+            modules = [
+              ({ pkgs, ... }: {
+                dotenv.enable = false;
+                dotenv.disableHint = true;
+
+                languages.javascript = {
+                  enable = true;
+                  package = pkgs.nodejs_20;
+                };
+
+                processes = { npm-dev.exec = "npm run dev"; };
+              })
+            ];
+          };
+        });
+    };
+}
